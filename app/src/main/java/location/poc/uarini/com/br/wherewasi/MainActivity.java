1494631b1411e679f50ae54dfe23050223d35ac3
Marcos Oliveira <marcos.oliveira@fpf.br>
2018-08-28 16:47:24 -0400

    first commit

---
+++ app/src/main/java/location/poc/uarini/com/br/wherewasi/MainActivity.java
@@ -0,0 +1,126 @@
+package location.poc.uarini.com.br.wherewasi;
+
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import location.poc.uarini.com.br.wherewasi.service.RegisterLocationService;
+
+/**
+ * https://github.com/googlesamples/android-play-location
+ */
+public class MainActivity extends AppCompatActivity  {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_PERMISSIONS_CODE = 12;
+
+    private boolean mShouldUnbind;
+
+    private RegisterLocationService mBoundService;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.findViewById(R.id.btLocation).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.registerLocation();
+            }
+        });
+
+        if (!this.hasPermission()) {
+            this.requestPermissions();
+        }
+
+        final Intent intent = new Intent(this, RegisterLocationService.class);
+        this.startService(intent);
+        this.doBindService();
+    }
+
+    private void registerLocation() {
+        if (mShouldUnbind) {
+            this.mBoundService.register();
+        }
+    }
+
+    private boolean hasPermission() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,  Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+
+        boolean shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (shouldProvideRationale) {
+            // SHOW EXPLAIN BEFORE REQUEST PERMISSIONS
+        }
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_PERMISSIONS_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+             //   this.registerLocation();
+            } else {
+                // TODO
+            }
+        }
+    }
+
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+              mBoundService = ((RegisterLocationService.LocalBinder) service).getService();
+
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+             mBoundService = null;
+
+        }
+    };
+
+    private void doBindService() {
+         if (this.bindService(new Intent(this, RegisterLocationService.class),  this.mConnection, Context.BIND_AUTO_CREATE)) {
+             this.mShouldUnbind = true;
+        } else {
+
+        }
+    }
+
+    void doUnbindService() {
+        if (this.mShouldUnbind) {
+            this.unbindService(this.mConnection);
+            this.mShouldUnbind = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.doUnbindService();
+    }
+
+}
---
+++ app/src/main/java/location/poc/uarini/com/br/wherewasi/service/RegisterLocationService.java
@@ -0,0 +1,124 @@
+package location.poc.uarini.com.br.wherewasi.service;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.GeoPoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterLocationService extends Service {
+
+    private static final String TAG = RegisterLocationService.class.getSimpleName();
+
+    private final IBinder mBinder = new LocalBinder();
+    private Criteria criteria;
+    private LocationManager locationManager;
+    private FirebaseFirestore db;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return this.mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+
+        final FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        this.db = FirebaseFirestore.getInstance();
+        this.db.setFirestoreSettings(settings);
+
+        this.locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        this.criteria = new Criteria();
+        this.criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        this.criteria.setPowerRequirement(Criteria.POWER_LOW);
+        this.criteria.setAltitudeRequired(false);
+        this.criteria.setBearingRequired(false);
+        this.criteria.setSpeedRequired(false);
+        this.criteria.setCostAllowed(true);
+        this.criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        this.criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+    }
+
+    public void register() {
+        Log.e(RegisterLocationService.class.getSimpleName(), "register");
+
+        if (ActivityCompat.checkSelfPermission(this,  Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestSingleUpdate(criteria, locationListener, null);
+        }
+    }
+
+    private void saveLocation(Location location) {
+        final Map<String, Object> user = new HashMap<>();
+        user.put("point", new GeoPoint(location.getLatitude(), location.getLongitude()));
+        db.collection("locations").add(user) .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.w(TAG, "Error adding document", e);
+            }
+        });
+
+    }
+
+
+    public class LocalBinder extends Binder {
+       public RegisterLocationService getService() {
+            return RegisterLocationService.this;
+        }
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i(RegisterLocationService.class.getSimpleName(), "onLocationChanged " + location.getProvider() );
+            saveLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+}
